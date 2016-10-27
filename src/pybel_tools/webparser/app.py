0be9d086cedad2d268537cf671127d7955853760
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-10-27 11:41:17 +0200

    Update docs

--- src/pybel_tools/webparser/app.py
+++ src/pybel_tools/webparser/app.py
@@ -8,16 +8,22 @@ from flask import Flask, request
 app = Flask(__name__)
 
 parser = pybel.parser.BelParser()
+parser.statement.setName('Relation Grammar')
 parser.statement.streamline()
 app.config['GRAPH'] = parser
 
-@app.route('/', methods=['GET'])
+
+@app.route('/', methods=['GET', 'POST'])
 def no_place_like_home():
     return app.send_static_file('index.html')
 
 
 @app.route('/parse/<statement>', methods=['GET', 'POST'])
 def parse(statement):
+    """Parses a statement that's been URL encoded. URL Parameters added as annotations to relation
+
+    :param statement: a URL encoded BEL statement
+    """
     parser = app.config['GRAPH']
     parser.control_parser.clear()
     parser.control_parser.annotations.update({
@@ -26,6 +32,7 @@ def parse(statement):
         'host': request.host,
         'user': request.remote_user
     })
+
     parser.control_parser.annotations.update(request.args)
     res = parser.statement.parseString(statement)
 
@@ -34,6 +41,7 @@ def parse(statement):
 
 @app.route('/dump')
 def dump():
+    """Dumps the content of the current graph as a PyBEL *.gpickle"""
     out_stream = io.BytesIO()
     pybel.to_pickle(app.config['GRAPH'].graph, out_stream)
     return flask.Response(out_stream.getvalue(), mimetype='text/plain')
@@ -41,6 +49,7 @@ def dump():
 
 @app.route('/clear')
 def clear():
+    """Clears the content of the current graph"""
     app.config['GRAPH'].clear()
     return "cleared"
 
