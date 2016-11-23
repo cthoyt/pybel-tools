020b919c568aab180d21e2e058d85321e976a63c
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-11-23 19:58:58 +0100

    Updates all around!!!
    - removed not real tests
    - updated owl parser (pybel has one so this is getting deprecated)
    - added namespace building to CLI

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,11 +1,24 @@
+import os
+import tempfile
+import unittest
+
 from pybel_tools.boilerplate import make_boilerplate
 from pybel_tools.merger import merge
 
-if __name__ == '__main__':
-    make_boilerplate('~/Desktop/boilerplate1.bel', 'BP1', 'cthoyt+1@gmail.com', 'Boilerplate Test Document 1',
-                     pmids=[26209472, 26940375])
 
-    make_boilerplate('~/Desktop/boilerplate2.bel', 'BP2', 'cthoyt+2@gmail.com', 'Boilerplate Test Document 2',
-                     pmids=[26839406, 26612754])
+class TestBoilerplate(unittest.TestCase):
+    def setUp(self):
+        self.dir = tempfile.mkdtemp()
 
-    merge('~/Desktop/bp_merge.bel', '~/Desktop/boilerplate1.bel', '~/Desktop/boilerplate2.bel')
+    def doCleanups(self):
+        os.remove(self.dir)
+
+    def test_1(self):
+        b1 = os.path.join(self.dir, 'boilerplate1.bel')
+        b2 = os.path.join(self.dir, 'boilerplate2.bel')
+        b3 = os.path.join(self.dir, 'bp_merge.bel')
+        make_boilerplate(b1, 'BP1', 'cthoyt+1@gmail.com', 'Boilerplate Test Document 1', pmids=[26209472, 26940375])
+
+        make_boilerplate(b2, 'BP2', 'cthoyt+2@gmail.com', 'Boilerplate Test Document 2', pmids=[26839406, 26612754])
+
+        merge(b3, b1, b2)
