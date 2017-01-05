23c1d3a8d3b132e7a5cd4eaa0a3db0cfae6f38c4
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-01-05 16:24:36 -0500

    Fix version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ author = u'Charles Tapley Hoyt'
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '0.2.7-dev'
+release = '0.0.1'
 
 # The short X.Y version.
 parsed_version = re.match(
