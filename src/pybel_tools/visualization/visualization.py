3f1762c2a79c891ad4988f16624378320446150f
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-02-07 13:58:30 +0100

    Update visualisation pipeline

--- src/pybel_tools/visualization/visualization.py
+++ src/pybel_tools/visualization/visualization.py
@@ -1,57 +1,68 @@
-import pybel
-from io import StringIO
+from __future__ import print_function
+
+import json
+import os
+
 import jinja2
 
-import os
-import json
+import pybel
 
-PATH = os.path.dirname(os.path.abspath(__file__))
+HERE = os.path.dirname(os.path.abspath(__file__))
+
 TEMPLATE_ENVIRONMENT = jinja2.Environment(
     autoescape=False,
-    loader=jinja2.FileSystemLoader(os.path.join(PATH, 'templates')),
-    trim_blocks=False)
-TEMPLATE_ENVIRONMENT.globals['STATIC_PREFIX'] = PATH + '/static/'
+    loader=jinja2.FileSystemLoader(os.path.join(HERE, 'templates')),
+    trim_blocks=False
+)
 
-
-def graph_to_json(graph):
-    """Returns json representation of the graph"""
-
-    # Tested in notebook
-    io = StringIO()
-
-    pybel.to_json(graph, io)
-
-    # to_json output needs to be modified (single quotes and backslashes)
-    io.getvalue()
-
-    return io
+TEMPLATE_ENVIRONMENT.globals['STATIC_PREFIX'] = HERE + '/static/'
 
 
 def render_template(template_filename, context):
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
 
-def create_graph_html(graph):
-    fname = "graph_visualization.html"
+def render_graph_template(context):
+    return render_template('graph_template.html', context)
 
-    context = {
-        'json': graph,
+
+def build_graph_context(graph):
+    """
+
+    :param graph:
+    :type graph: pybel.BELGraph
+    :return:
+    """
+    graph_json_str = pybel.io.to_jsons(graph)
+
+    return {
+        'json': graph_json_str,
         'number_nodes': '10',
         'number_edges': '32434'
     }
 
-    with open(fname, 'w') as f:
-        html = render_template('graph_template.html', context)
-        f.write(html)
+
+def to_html(graph):
+    """Creates an HTML visualization for the given JSON representation of a BEL graph
+
+    :param graph:
+    :type graph:
+    :return:
+    """
+    return render_graph_template(build_graph_context(graph))
 
 
 def main():
-    with open('graph.json') as data_file:
-        data = json.load(data_file)
-    create_graph_html(data)
+    with open(os.path.expanduser('~/Desktop/graph.json')) as f:
+        data = json.load(f)
 
+    with open(os.path.expanduser('~/Desktop/test.html'), 'w') as f:
+        print(render_graph_template({
+            'json': json.dumps(data),
+            'number_nodes': '10',
+            'number_edges': '32434'
+        }), file=f)
 
-########################################
 
 if __name__ == "__main__":
     main()
