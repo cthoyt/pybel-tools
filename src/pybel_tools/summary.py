912e42e226d006e487190de66103edd6a733a55f
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-03-05 10:51:57 +0100

    Update plot summary and documentation
    [skip ci]

--- src/pybel_tools/summary.py
+++ src/pybel_tools/summary.py
@@ -56,7 +56,7 @@ def get_names(graph, namespace):
 def get_unique_names(graph):
     """Get a dictionary of {namespace: set of names} present in the graph
 
-    :param graph:
+    :param graph: A BEL Graph
     :type graph: BELGraph
     :return: A dictionary of {namespace: set of names}
     :rtype: dict
@@ -284,19 +284,33 @@ def calculate_error_by_annotation(graph, annotation):
     return results
 
 
-# Visualization with Matplotlib
+# Visualization with matplotlib
 
-def plot_summary(graph, plt, figsize=(11, 4), **kwargs):
-    """Plots your graph summary statistics. After, you need to run :code:`plt.show()`.
+def plot_summary_axes(graph, lax, rax):
+    """Plots your graph summary statistics on the given axes.
 
+    After, you should run :code:`plt.tight_layout()` and you must run :code:`plt.show()` to view.
+
+    Shows:
     1. Count of nodes, grouped by function type
     2. Count of edges, grouped by relation type
 
     :param graph: A BEL graph
     :type graph: BELGraph
-    :param plt: Give :code:`matplotlib.pyplot` to this parameter
+    :param lax: An axis object from matplotlib
+    :param rax: An axis object from matplotlib
+
+    Example usage:
+
+    >>> import matplotlib.pyplot as plt
+    >>> from pybel import from_pickle
+    >>> from pybel_tools.summary import plot_summary_axes
+    >>> graph = from_pickle('~/dev/bms/aetionomy/parkinsons.gpickle')
+    >>> fig, axes = plt.subplots(1, 2, figsize=(16, 4))
+    >>> plot_summary_axes(graph, axes[0], axes[1])
+    >>> plt.tight_layout()
+    >>> plt.show()
     """
-    fig, axes = plt.subplots(1, 2, figsize=figsize, **kwargs)
 
     ntc = count_functions(graph)
     etc = count_relations(graph)
@@ -304,10 +318,42 @@ def plot_summary(graph, plt, figsize=(11, 4), **kwargs):
     df = pd.DataFrame.from_dict(ntc, orient='index')
     df_ec = pd.DataFrame.from_dict(etc, orient='index')
 
-    df.sort_values(0, ascending=True).plot(kind='barh', logx=True, ax=axes[0])
-    axes[0].set_title('Number Nodes: {}'.format(graph.number_of_nodes()))
+    df.sort_values(0, ascending=True).plot(kind='barh', logx=True, ax=lax)
+    lax.set_title('Number Nodes: {}'.format(graph.number_of_nodes()))
 
-    df_ec.sort_values(0, ascending=True).plot(kind='barh', logx=True, ax=axes[1])
-    axes[1].set_title('Number Edges: {}'.format(graph.number_of_edges()))
+    df_ec.sort_values(0, ascending=True).plot(kind='barh', logx=True, ax=rax)
+    rax.set_title('Number Edges: {}'.format(graph.number_of_edges()))
 
+
+def plot_summary(graph, plt, figsize=(11, 4), **kwargs):
+    """Plots your graph summary statistics. This function is a thin wrapper around :code:`plot_summary_axis`. It
+    automatically takes care of building figures given matplotlib's pyplot module as an argument. After, you need
+    to run :code:`plt.show()`.
+
+    :code:`plt` is given as an argument to avoid needing matplotlib as a dependency for this function
+
+    Shows:
+
+    1. Count of nodes, grouped by function type
+    2. Count of edges, grouped by relation type
+
+    :param graph: A BEL graph
+    :type graph: BELGraph
+    :param plt: Give :code:`matplotlib.pyplot` to this parameter
+
+    Example usage:
+
+    >>> import matplotlib.pyplot as plt
+    >>> from pybel import from_pickle
+    >>> from pybel_tools.summary import plot_summary
+    >>> graph = from_pickle('~/dev/bms/aetionomy/parkinsons.gpickle')
+    >>> plot_summary(graph, plt, figsize=(16, 4))
+    >>> plt.show()
+
+    """
+    fig, axes = plt.subplots(1, 2, figsize=figsize, **kwargs)
+    lax = axes[0]
+    rax = axes[1]
+
+    plot_summary_axes(graph, lax, rax)
     plt.tight_layout()
