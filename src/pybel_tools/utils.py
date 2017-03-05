90d868149db7e330c2aec744cd3b8006775cd795
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-03-05 10:36:09 +0100

    Update docs
    [skip ci]

--- src/pybel_tools/utils.py
+++ src/pybel_tools/utils.py
@@ -1,11 +1,18 @@
 from collections import defaultdict, Counter
 
+from pybel import BELGraph
 from pybel.constants import RELATION, ANNOTATIONS, FUNCTION, PATHOLOGY
 
 
 # TODO rename this function
 def graph_content_transform(graph):
-    """Builds a dictionary of {node pair: set of edge types}"""
+    """Builds a dictionary of {node pair: set of edge types}
+
+    :param graph: A BEL Graph
+    :type graph: BELGraph
+    :return: A dictionary of {(node, node): set of edge types}
+    :rtype dict
+    """
     edge_relations = defaultdict(set)
     for u, v, d in graph.edges_iter(data=True):
         edge_relations[u, v].add(d[RELATION])
@@ -13,12 +20,23 @@ def graph_content_transform(graph):
 
 
 def count_defaultdict(d):
-    """Takes a defaultdict(list) and applys a counter to each list"""
+    """Takes a defaultdict(list) and applys a counter to each list
+
+    :param d: A dictionary of lists
+    :type d: defaultdict(list)
+    :return: A dictionary of {key: Counter(values)}
+    :rtype: dict
+    """
     return {k: Counter(v) for k, v in d.items()}
 
 
 def count_dict_values(dict_of_counters):
-    """Counts the number of elements in each value (can be list, Counter, etc)"""
+    """Counts the number of elements in each value (can be list, Counter, etc)
+
+    :param d: A dictionary of lists
+    :type d: defaultdict(list)
+    :rtype: dict
+    """
     return {k: len(v) for k, v in dict_of_counters.items()}
 
 
@@ -52,6 +70,15 @@ def keep_node(graph, node, super_nodes=None):
 
     This function returns false for nodes that have PATHOLOGY or are on a pre-defined blacklist. This can be most
     easily used with :py:func:`functools.partial`:
+
+    :param graph: A BEL Graph
+    :type graph: BELGraph
+    :param node: The node to check if it should be kepy
+    :type node: tuple
+    :param super_nodes: A list of nodes to automatically throw out
+    :type super_nodes: list of tuples
+    :return: Should the node be kept?
+    :rtype: bool
 
 
     >>> from functools import partial
