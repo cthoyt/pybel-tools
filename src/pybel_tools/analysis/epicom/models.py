188ad741d5380b27d071ed9ff2a24a92d982a5d1
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-09-06 15:20:24 +0200

    Update epicom code

--- src/pybel_tools/analysis/epicom/models.py
+++ src/pybel_tools/analysis/epicom/models.py
@@ -4,8 +4,8 @@ from sqlalchemy import Column, Float, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 
 from pybel.manager.models import (
-    ANNOTATION_TABLE_NAME, AnnotationEntry, Base, NAME_TABLE_NAME,
-    NETWORK_TABLE_NAME, NamespaceEntry, Network,
+    Base, NAMESPACE_TABLE_NAME, NAME_TABLE_NAME, NETWORK_TABLE_NAME, NamespaceEntry,
+    Network,
 )
 
 TABLE_BASE_NAME = 'epicom'
@@ -13,7 +13,7 @@ SCORE_TABLE_NAME = 'epicom_score'
 
 
 class Score(Base):
-    """Represents the score for a subgraph in a given network"""
+    """Represents the score for a sub-graph in a given network"""
     __tablename__ = SCORE_TABLE_NAME
 
     id = Column(Integer, primary_key=True)
@@ -21,8 +21,8 @@ class Score(Base):
     network_id = Column(Integer, ForeignKey('{}.id'.format(NETWORK_TABLE_NAME)))
     network = relationship(Network)
 
-    annotation_id = Column(Integer, ForeignKey('{}.id'.format(ANNOTATION_TABLE_NAME)))
-    annotation = relationship(AnnotationEntry)
+    annotation_id = Column(Integer, ForeignKey('{}.id'.format(NAMESPACE_TABLE_NAME)))
+    annotation = relationship(NamespaceEntry)
 
     drug_id = Column(Integer, ForeignKey('{}.id'.format(NAME_TABLE_NAME)))
     drug = relationship(NamespaceEntry)
