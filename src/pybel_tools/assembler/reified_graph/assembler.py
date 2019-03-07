e21b4d1b399b9e9e6f95b8ee260de1f1cb521e08
lacerda <s0mapiod@uni-bonn.de>
2019-03-07 22:47:01 +0100

    Implementing PhosphorylationConverter

--- src/pybel_tools/assembler/reified_graph/assembler.py
+++ src/pybel_tools/assembler/reified_graph/assembler.py
@@ -2,13 +2,16 @@
 
 """Utilities to assemble a BEL graph as bipartite graph of nodes and reified edges."""
 
+import logging
 import unittest
 
 import networkx as nx
 
+from abc import ABC, abstractmethod
 from pybel import BELGraph
-from pybel.dsl import activity, pmod, protein
+from pybel.dsl import activity, pmod, protein, BaseEntity
 from pybel.testing.utils import n
+from typing import Dict, Optional, Tuple
 
 __all__ = [
     'reify_bel_graph',
@@ -18,9 +21,93 @@ SUBJECT = 'subject'
 OBJECT = 'object'
 
 
+class ReifiedConverter(ABC):
+    """Base class for BEL -> Reified edges graph conversion"""
+
+    @staticmethod
+    @abstractmethod
+    def predicate(u: BaseEntity, v: BaseEntity, key: str, edge_data: Dict) -> bool:
+        """Test if a BEL edge corresponds to the converter."""
+
+    @staticmethod
+    @abstractmethod
+    def convert(u: BaseEntity, v: BaseEntity, key: str, edge_data: Dict) \
+            -> Tuple[BaseEntity, Tuple[int, str], BaseEntity]:
+        """Convert a BEL edge to a reified edge."""
+
+
+class IntermediateConverter(ReifiedConverter):
+    """Implements the convert method"""
+
+    target_relation = ...
+
+    @classmethod
+    def convert(cls, u: BaseEntity, v: BaseEntity, key: str, edge_data: Dict) \
+            -> Tuple[BaseEntity, Tuple[int, str], BaseEntity]:
+        # TODO create vertex X
+        # TODO i need an autoincrement somewhere
+        pred_vertex = (0, cls.target_relation)
+        # TODO v loses pmod(Ph)?
+        # mod_v = v.deepcopy()
+        object_edge = (pred_vertex, OBJECT, v)
+        # TODO ask what to do if it is a decrease of the phosphorylation
+
+        return u, pred_vertex, v
+
+
+class PhosphorylationConverter(IntermediateConverter):
+    """Converts BEL statements of the form A B p(C, pmod(Ph))"""
+
+    target_relation = "phosphorylates"
+
+    @classmethod
+    def predicate(cls, u: BaseEntity, v: BaseEntity, key: str, edge_data: Dict) -> bool:
+        if "variant" in v:
+            print("there is a variant")
+
+        return "relation" in edge_data and edge_data['relation'] in ['directlyIncreases', 'increases']
+
+
+def reify_edge(u: BaseEntity, v: BaseEntity, key: str, edge_data: Dict) \
+    -> Optional[Tuple[BaseEntity, Tuple[int, str], BaseEntity]]:
+
+    converters = [
+        PhosphorylationConverter
+    ]
+    for converter in converters:
+        if converter.predicate(u, v, key, edge_data):
+            return converter.convert(u, v, key, edge_data)
+
+    logging.warning(f"No converter found for {u}, {v}")
+
+    # No converter found
+    return None
+
+
 def reify_bel_graph(bel_graph: BELGraph) -> nx.DiGraph:
     """Generate a new graph with reified edges."""
-    raise NotImplementedError
+
+    reified_graph = nx.DiGraph()
+
+    for edge in bel_graph.edges(keys=True):
+        if len(edge) == 2:
+            (u, v) = edge
+            key = None
+            data = bel_graph[u][v]
+        elif len(edge) == 3:
+            (u, v, key) = edge
+            data = bel_graph[u][v][key]
+        print(u)
+        print(v)
+        print(data)
+
+        reified_edge = reify_edge(u, v, key, data)
+        if reified_edge:
+            new_u, reif_edge, new_v = reified_edge
+            reified_graph.add_edge(new_u, reif_edge, label=SUBJECT)
+            reified_graph.add_edge(reif_edge, new_v, label=OBJECT)
+
+    return reified_graph
 
 
 cdk5 = protein('HGNC', 'CDK5', 'HGNC:1774')
