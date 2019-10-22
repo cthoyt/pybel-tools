79c0b94058883cfc36d3710845b604acedbcf8f7
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-10-22 19:29:29 +0200

    Update usages of add_node_from_data

--- tests/test_mutation/test_collapse.py
+++ tests/test_mutation/test_collapse.py
@@ -19,11 +19,11 @@ CHEBI = 'CHEBI'
 
 g1 = Gene(HGNC, '1')
 r1 = rna(HGNC, '1')
-p1_data = Protein(HGNC, '1')
+p1 = Protein(HGNC, '1')
 
 g2 = Gene(HGNC, '2')
 r2 = rna(HGNC, '2')
-p2_data = Protein(HGNC, '2')
+p2 = Protein(HGNC, '2')
 
 g3 = Gene(HGNC, '3')
 r3 = rna(HGNC, '3')
@@ -32,18 +32,18 @@ p3 = Protein(HGNC, '3')
 g4 = Gene(HGNC, '4')
 m4 = mirna(HGNC, '4')
 
-a5_data = abundance(CHEBI, '5')
-p5_data = pathology(GO, '5')
+a5 = abundance(CHEBI, '5')
+p5 = pathology(GO, '5')
 
 
 class TestCollapseProteinInteractions(unittest.TestCase):
     def test_protein_interaction_1(self):
         graph = BELGraph()
 
-        p1 = graph.add_node_from_data(p1_data)
-        p2 = graph.add_node_from_data(p2_data)
-        a5 = graph.add_node_from_data(a5_data)
-        p5 = graph.add_node_from_data(p5_data)
+        graph.add_node_from_data(p1)
+        graph.add_node_from_data(p2)
+        graph.add_node_from_data(a5)
+        graph.add_node_from_data(p5)
 
         graph.add_qualified_edge(p1, p2, relation=POSITIVE_CORRELATION, citation=n(), evidence=n())
 
@@ -61,10 +61,10 @@ class TestCollapseProteinInteractions(unittest.TestCase):
     def test_protein_interaction_2(self):
         graph = BELGraph()
 
-        p1 = graph.add_node_from_data(p1_data)
-        p2 = graph.add_node_from_data(p2_data)
-        a5 = graph.add_node_from_data(a5_data)
-        p5 = graph.add_node_from_data(p5_data)
+        graph.add_node_from_data(p1)
+        graph.add_node_from_data(p2)
+        graph.add_node_from_data(a5)
+        graph.add_node_from_data(p5)
 
         graph.add_qualified_edge(p1, p2, relation=POSITIVE_CORRELATION, citation=n(), evidence=n())
         graph.add_qualified_edge(p1, p2, relation=ASSOCIATION, citation=n(), evidence=n())
